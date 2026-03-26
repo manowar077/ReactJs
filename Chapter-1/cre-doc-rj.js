855677f65180c5b318bac318250938817691c954
Md Manowar Alam <mdmanowar0999@gmail.com>
2026-03-25 23:23:49 -0700

    first commit

---
+++ Chapter-1/cre-doc-rj.js
@@ -0,0 +1,27 @@
+/*
+@libary (give features to the website)
+ ---GASAP -"animation (for moving animation)"
+ ---Lenis -"used for smooth scrolling "
+ ---reactjs -"used for making ui of wbsite"
+@framework
+----nextjs - 
+----Angular
+
+ */
+
+/*
+
+export -2
+---named(import exactly the specified target)
+[export const b=20
+
+then import as
+import {b} from './filenmae'
+]
+---default
+*/
+const a=20
+
+//export default a
+const arr=[22,34,45,55]
+export default arr 
