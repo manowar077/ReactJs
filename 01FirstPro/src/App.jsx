ae0551a736e48a6da70667db4811965d560575b2
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-01 11:15:20 -0700

    update

--- 01FirstPro/src/App.jsx
+++ 01FirstPro/src/App.jsx
@@ -1,6 +1,14 @@
  import React from "react"
  function App(){
-  return <h1>Hello World! Updated</h1>
+    return(
+        //fragments
+        <>
+        <div id="Dad">
+            <h1>First React App</h1>
+        </div>
+        <div id="chacha"> welcome to my web</div>
+        </>
+    )
  }
 
  export default App
