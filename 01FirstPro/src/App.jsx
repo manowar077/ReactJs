cce47f9cb3c2d6520993801b0e8be5717e5790e6
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-01 11:45:00 -0700

    update

--- 01FirstPro/src/App.jsx
+++ 01FirstPro/src/App.jsx
@@ -1,13 +1,21 @@
  import React from "react"
+ import ReactDOM from 'react-dom/client'
  function App(){
     return(
-        //fragments
+       /* //fragments
         <>
         <div id="Dad">
             <h1>First React App</h1>
         </div>
         <div id="chacha"> welcome to my web</div>
-        </>
+        </>*/
+        <>
+         <div className="card">
+            <h1>Md Manowar Alam</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam cumque porro eligendi maxime illum tempora? Adipisci blanditiis minus dignissimos, obcaecati porro eligendi maiores eveniet cumque beatae eos totam assumenda.</p>
+        </div>
+</>
+        
     )
  }
 
