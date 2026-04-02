3bfb2122abf4a839667043fb73f593a52d1ab4f7
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-01 21:50:44 -0700

    update

--- 01FirstPro/src/App.jsx
+++ 01FirstPro/src/App.jsx
@@ -2,6 +2,7 @@
  import card from "./component/card"
  import ReactDOM from 'react-dom/client'
  function App(){
+    const name = 'Manowar Alam';
     return(
        /* //fragments
         <>
@@ -10,7 +11,9 @@
         </div>
         <div id="chacha"> welcome to my web</div>
         </>*/
-        <>
+<>
+        ;
+        <h1>My name is {name}</h1>
          <div className="card">
             <h1>Md Manowar Alam</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam cumque porro eligendi maxime illum tempora? Adipisci blanditiis minus dignissimos, obcaecati porro eligendi maiores eveniet cumque beatae eos totam assumenda.</p>
--- 01FirstPro/src/component/card.jsx
+++ 01FirstPro/src/component/card.jsx
@@ -1,4 +1,29 @@
 function card(){
-    return 'hi i am card';
+    return(
+        < >
+        <div className="hero">
+            <div className="card">
+            <h1>Md Manowar Alam</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam cumque porro eligendi maxime illum tempora? Adipisci blanditiis minus dignissimos, obcaecati porro eligendi maiores eveniet cumque beatae eos totam assumenda.</p>
+        </div>
+        <div className="card">
+            <h1>Md Zeeshan Quadri</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam cumque porro eligendi maxime illum tempora? Adipisci blanditiis minus dignissimos, obcaecati porro eligendi maiores eveniet cumque beatae eos totam assumenda.</p>
+        </div>
+        <div className="card">
+            <h1>Md Manowar Alam</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam cumque porro eligendi maxime illum tempora? Adipisci blanditiis minus dignissimos, obcaecati porro eligendi maiores eveniet cumque beatae eos totam assumenda.</p>
+        </div>
+        <div className="card">
+            <h1>Md Manowar Alam</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam cumque porro eligendi maxime illum tempora? Adipisci blanditiis minus dignissimos, obcaecati porro eligendi maiores eveniet cumque beatae eos totam assumenda.</p>
+        </div>
+        </div>
+         
+        </>
+        
+          
+        )
+      
 }
 export default card;
