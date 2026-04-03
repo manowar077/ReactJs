1a37e2f7295242486c4582c071853aaa09733243
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-03 21:17:51 +0530

    update

--- 02firstpro/src/App.jsx
+++ 02firstpro/src/App.jsx
@@ -9,6 +9,19 @@ const App = () => {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cupiditate facere ab eaque voluptatibus aliquam, doloremque illum accusamus veritatis accusantium quis, ipsam, culpa praesentium nostrum tempora molestiae rem. Labore, excepturi.</p>
         <button>view Profile</button>
       </div>
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1769406525803-9ea06bd97577?q=80&w=1481&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Md Manowar Alam</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cupiditate facere ab eaque voluptatibus aliquam, doloremque illum accusamus veritatis accusantium quis, ipsam, culpa praesentium nostrum tempora molestiae rem. Labore, excepturi.</p>
+        <button>view Profile</button>
+      </div>
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1769406525803-9ea06bd97577?q=80&w=1481&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Md Manowar Alam</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cupiditate facere ab eaque voluptatibus aliquam, doloremque illum accusamus veritatis accusantium quis, ipsam, culpa praesentium nostrum tempora molestiae rem. Labore, excepturi.</p>
+        <button>view Profile</button>
+      </div>
+      
     </div>
   )
 }
